36089ce2ef3b9e42b668ff2848101cb80a2dab28
tm-qc <taka.hi.rock.music.lovers@gmail.com>
2026-06-21 08:04:04 +0000

    暫定作成：ロール（認可）：Userポリシー作成
    ロールの役割分け実装内容想定
    
    	管理者
    		・ユーザー一覧
    		・ユーザー登録
    		・ユーザー編集
    		・ユーザー削除
    
    	一般
    		・自分のユーザー編集
    
    - ポリシークラス
    
    php artisan make:policy UserPolicy --model=User
    ※--model=User を付けると、viewAny、view、create、update、delete など、CRUD系のPolicyメソッドが入った形で作られます

---
+++ src/app/Policies/UserPolicy.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+
+class UserPolicy
+{
+    /**
+     * ユーザー一覧を表示できるか判定する
+     *
+     * 管理者だけ許可する。
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->isAdmin();
+    }
+
+    /**
+     * ユーザー詳細を表示できるか判定する
+     *
+     * 管理者:
+     * - 全ユーザーを表示できる
+     *
+     * 一般:
+     * - 自分自身のみ表示できる
+     */
+    public function view(User $user, User $targetUser): bool
+    {
+        return $user->isAdmin()
+            || $user->id === $targetUser->id;
+    }
+
+
+    /**
+     * ユーザーを新規登録できるか判定する
+     *
+     * 管理者:
+     * - ユーザー登録できる
+     *
+     * 一般:
+     * - ユーザー登録できない
+     */
+    public function create(User $user): bool
+    {
+        return $user->isAdmin();
+    }
+
+    /**
+     * ユーザーを更新できるか判定する
+     *
+     * 管理者:
+     * - 全ユーザーを更新できる
+     *
+     * 一般:
+     * - 自分自身のみ更新できる
+     */
+    public function update(User $user, User $targetUser): bool
+    {
+        return $user->isAdmin()
+            || $user->id === $targetUser->id;
+    }
+
+    /**
+     * ユーザーを削除できるか判定する
+     * ※soft delete実装済みの場合は論理削除
+     *
+     * 管理者:
+     * - ユーザー削除できる
+     *
+     * 一般:
+     * - ユーザー削除できない
+     */
+    public function delete(User $user, User $targetUser): bool
+    {
+        return $user->isAdmin();
+    }
+
+    /**
+     * ユーザーを物理削除できるか判定する
+     *
+     * 物理削除は管理者だけ許可する。
+     *
+     */
+    public function forceDelete(User $user, User $targetUser): bool
+    {
+        return $user->isAdmin();
+    }
+
+    /**
+     * 論理削除済みユーザーを復元できるか判定する
+     *
+     * 復元は管理者だけ許可する
+     */
+    // public function restore(User $user, User $targetUser): bool
+    // {
+    //     return $user->isAdmin();
+    // }
+
+}
