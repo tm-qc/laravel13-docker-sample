c9a329983f1ca65a1513153f636482aad487467a
tm-qc <taka.hi.rock.music.lovers@gmail.com>
2026-06-21 15:25:29 +0000

    ヘッダーにログイン情報追加

--- src/resources/views/components/header.blade.php
+++ src/resources/views/components/header.blade.php
@@ -7,10 +7,32 @@
 
         {{-- ログイン中だけ表示 --}}
         @auth
+            @php
+                $loginUser = auth()->user();
+            @endphp
+            {{-- 不要だと思うが、一般で入ったときにポリシーが聞いてるか確認になるので、いったんこのまま --}}
             <nav class="site-nav">
                 <a href="{{ route('users.index') }}">ユーザー一覧</a>
                 <a href="{{ route('users.create') }}">ユーザー登録</a>
             </nav>
+
+            {{-- ログインユーザー情報 --}}
+            <div class="login-user-info">
+                <span class="login-user-label">【ログイン中】：</span>
+
+                {{--
+                    ユーザー名を表示する
+                    {{ }} はHTMLエスケープされるため、XSS対策として安全に表示できる。
+                --}}
+                <span class="login-user-name">{{ $loginUser->name }}</span>
+
+                <span class="login-user-label">【ロール】：</span>
+                {{-- ロール名 --}}
+                {{-- ほぼないが念のため 'ロール未設定' --}}
+                <span class="login-user-role">
+                    {{ $loginUser->role?->name ?? 'ロール未設定' }}
+                </span>
+            </div>
 
             <form action="{{ route('logout') }}" method="post" class="logout-form">
                 @csrf
