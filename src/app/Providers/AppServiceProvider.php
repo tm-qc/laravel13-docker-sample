afedaf868259bc8a3d0d9de4d697b3544e2e2d1c
tm-qc <taka.hi.rock.music.lovers@gmail.com>
2026-06-23 01:09:07 +0000

    サービスプロバイダー：コメント更新

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -11,14 +11,31 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
-    }
+        // アプリ起動時にサービスコンテナへの登録(バインド)を行う
 
+        // ※ 今回は省略してるインターフェースを使う場合の例
+        // $this->app->bind(
+        //     UserRepositoryInterface::class,
+        //     UserRepository::class
+        // );
+
+    }
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        //
+         // アプリケーション起動時の共通設定を行う
+
+        // サービスプロバイダが登録された後に行いたい処理などをかく
+        /**
+         * 本番環境ではURL生成をHTTPSに固定する。
+         *
+         * ロードバランサーやリバースプロキシ配下で、
+         * http URL が生成される問題を防ぐために使うことがある。
+         */
+        // if (app()->environment('production')) {
+        //     URL::forceScheme('https');
+        // }
     }
 }
